21a577309c90d6b47a8650426f96e11675e5658e
IgorPerikov <perikov.igor@gmail.com>
2016-12-26 20:53:18 +0300

    add akka-http implementation

---
+++ akka/src/main/java/org/clayman/comparison/akka/Server.java
@@ -0,0 +1,45 @@
+package org.clayman.comparison.akka;
+
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.http.scaladsl.model.StatusCode;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
+
+public class Server extends AllDirectives {
+
+    public static void main(String[] args) throws Exception {
+        ActorSystem system = ActorSystem.create("routes");
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        Server app = new Server();
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
+                ConnectHttp.toHost("localhost", 8080), materializer);
+
+
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+    }
+
+    private Route createRoute() {
+        return route(
+                path("ping", () ->
+                        get(() ->
+                                complete(StatusCode.int2StatusCode(200), "OK"))));
+    }
+}
